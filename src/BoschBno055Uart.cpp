acdff7c2cbac35399c8150f28db0b0d433e51cf6
Christian Holl <cyborgx1@gmail.com>
2016-04-26 17:35:09 -0400

    add almost everything ...

--- src/BoschBno055Uart.cpp
+++ src/BoschBno055Uart.cpp
@@ -1,5 +1,12 @@
 
 #include "BoschBno055Uart.h"
+#include <netinet/in.h>
+
+
+#define    ACCEL_FCT 1000.0
+#define    MAG_FCT 16.0
+#define    GYRO_FCT 900.0
+
 
 /**
  * @todo add license and move to source file
@@ -14,20 +21,40 @@ namespace h4r_bosch_bno055_uart
 BoschBno055Uart::BoschBno055Uart()
 :n_()
 ,nh_("~")
+,serial_()
+,serial_timeout_(serial::Timeout::simpleTimeout(50))
+,pub_imu_raw_(nh_.advertise<sensor_msgs::Imu>("imu_raw",1,false))
+,pub_imu_(nh_.advertise<sensor_msgs::Imu>("imu",1,false))
+,pub_mag_(nh_.advertise<sensor_msgs::MagneticField>("mag",1,false))
+,pub_temp_(nh_.advertise<sensor_msgs::Temperature>("temp",1,false))
 {
   nh_.param("rate", rate_, 10.0);
   std::string frame_id="/bosch_imu";
   nh_.param("frame_id", frame_id);
+  msg_imu_.header.frame_id=frame_id;
+  msg_imu_raw_.header.frame_id=frame_id;
+  msg_mag_.header.frame_id=frame_id;
+  msg_temperature_.header.frame_id=frame_id;
+
+  //Init covariances
+  msg_imu_.orientation_covariance[0]=-1;
+  msg_imu_.angular_velocity_covariance[0]=-1;
+  msg_imu_.linear_acceleration_covariance[0]=-1;
+
+  msg_imu_raw_.orientation_covariance[0]=-1;
+  msg_imu_raw_.angular_velocity_covariance[0]=-1;
+  msg_imu_raw_.linear_acceleration_covariance[0]=-1;
 
   std::string serial_device;
   nh_.param("serial_device", serial_device);
-  msg_imu_.header.frame_id=frame_id;
+
 
   serial_.setBaudrate(115200);
   serial_.setBytesize(serial::eightbits);
   serial_.setFlowcontrol(serial::flowcontrol_none);
   serial_.setStopbits(serial::stopbits_one);
   serial_.setPort(serial_device);
+  serial_.setTimeout(serial_timeout_);
 }
 
 BoschBno055Uart::~BoschBno055Uart()
@@ -41,9 +68,118 @@ int BoschBno055Uart::run()
 	ros::Rate loop_rate(rate_);
 
 	serial_.open();
-	while(ros::ok() && serial_.isOpen())
-	{
+	serial_.flush(); //flush io buffers
 
+	if(writeIMUByte(PAGE_ID,1))
+	{
+		ROS_ERROR("Could not write register page 1");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_OPR_MODE,OP_MODE_CONFIG))
+	{
+		ROS_ERROR("Could not write config mode");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_PWR_MODE,PWR_MODE_NORMAL))
+	{
+		ROS_ERROR("Could not write normal power mode");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE_ID,0))
+	{
+		ROS_ERROR("Could not write register page 0");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_UNIT_SEL,0x83))
+	{
+		ROS_ERROR("Could not write units");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_AXIS_MAP_CONFIG,0x24))
+	{
+		ROS_ERROR("Could not write axis mapping");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_AXIS_MAP_SIGN,0x06))
+	{
+		ROS_ERROR("Could not write axis sign");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_OPR_MODE,OP_MODE_NDOF))
+	{
+		ROS_ERROR("Could not write operation mode NDOF");
+		exit(1);
+	}
+
+	if(writeIMUByte(PAGE0_SYS_TRIGGER,0x00))
+	{
+		ROS_ERROR("Could not write sys trigger");
+		exit(1);
+	}
+
+
+	ImuData imu_data;
+	while(ros::ok())
+	{
+		if(!serial_.isOpen())
+		{
+			ROS_ERROR("Serial not opened!");
+			exit(1);
+		}
+
+
+		if(!readIMU(PAGE0_ACC_DATA_X_LSB,(uint8_t*)&imu_data,46))
+		{
+
+			//IMU raw
+			msg_imu_raw_.header.stamp=ros::Time::now();
+			msg_imu_raw_.linear_acceleration.x=((double)ntohs(imu_data.imu.AccelerationDataX))/ACCEL_FCT;
+			msg_imu_raw_.linear_acceleration.y=((double)ntohs(imu_data.imu.AccelerationDataY))/ACCEL_FCT;
+			msg_imu_raw_.linear_acceleration.z=((double)ntohs(imu_data.imu.AccelerationDataZ))/ACCEL_FCT;
+			msg_imu_raw_.angular_velocity.x=((double)ntohs(imu_data.imu.GyroscopeDataX))/GYRO_FCT;
+			msg_imu_raw_.angular_velocity.y=((double)ntohs(imu_data.imu.GyroscopeDataY))/GYRO_FCT;
+			msg_imu_raw_.angular_velocity.z=((double)ntohs(imu_data.imu.GyroscopeDataZ))/GYRO_FCT;
+
+			//IMU filtered
+			msg_imu_.header.stamp=ros::Time::now();
+			msg_imu_.linear_acceleration.x=((double)ntohs(imu_data.imu.LinearAccelerationDataX))/ACCEL_FCT;
+			msg_imu_.linear_acceleration.y=((double)ntohs(imu_data.imu.LinearAccelerationDataY))/ACCEL_FCT;
+			msg_imu_.linear_acceleration.z=((double)ntohs(imu_data.imu.LinearAccelerationDataZ))/ACCEL_FCT;
+			msg_imu_.angular_velocity.x=((double)ntohs(imu_data.imu.GyroscopeDataX))/GYRO_FCT;
+			msg_imu_.angular_velocity.y=((double)ntohs(imu_data.imu.GyroscopeDataY))/GYRO_FCT;
+			msg_imu_.angular_velocity.z=((double)ntohs(imu_data.imu.GyroscopeDataZ))/GYRO_FCT;
+			msg_imu_.orientation.x=((double)ntohs(imu_data.imu.QuaternionxData));
+			msg_imu_.orientation.y=((double)ntohs(imu_data.imu.QuaternionyData));
+			msg_imu_.orientation.z=((double)ntohs(imu_data.imu.QuaternionzData));
+			msg_imu_.orientation.w=((double)ntohs(imu_data.imu.QuaternionwData));
+
+			//Magnetometer
+			msg_mag_.header.stamp=ros::Time::now();
+			msg_mag_.magnetic_field.x=((double)ntohs(imu_data.imu.MagnetometerDataX))/MAG_FCT;
+			msg_mag_.magnetic_field.y=((double)ntohs(imu_data.imu.MagnetometerDataY))/MAG_FCT;
+			msg_mag_.magnetic_field.z=((double)ntohs(imu_data.imu.MagnetometerDataZ))/MAG_FCT;
+
+			//Temperature
+			msg_temperature_.header.stamp=ros::Time::now();
+			msg_temperature_.temperature=imu_data.temperature;
+
+
+			pub_imu_.publish(msg_imu_);
+			pub_imu_raw_.publish(msg_imu_raw_);
+			pub_mag_.publish(msg_mag_);
+			pub_temp_.publish(msg_temperature_);
+		}
+		else
+		{
+			ROS_ERROR("Could not read imu data!");
+		}
 
 
 
@@ -51,6 +187,57 @@ int BoschBno055Uart::run()
 		loop_rate.sleep();
 	}
 	return 0;
+}
+
+bool BoschBno055Uart::writeIMU(BNO055Register reg,const void *data, unsigned length)
+{
+	std::vector<uint8_t>out_data;
+	out_data.push_back(0xAA);
+	out_data.push_back(0); //WRITE
+	out_data.push_back((uint8_t)reg);
+	out_data.push_back(length);
+	for (int b = 0; b < length; ++b)
+	{
+		out_data.push_back(*((uint8_t*)data+b));
+	}
+
+	serial_.flushInput();
+	serial_.write(out_data);
+	serial_.flushOutput();
+
+	uint8_t in_data[2]={0,0};
+
+	if(serial_.read(in_data,2)==2)
+	{
+		return !(in_data[0]==0xBB && in_data[1]==0x01);
+	}
+
+	return true;
+}
+
+bool BoschBno055Uart::readIMU(BNO055Register reg, uint8_t *data, unsigned length)
+{
+	std::vector<uint8_t>out_data;
+		out_data.push_back(0xAA);
+		out_data.push_back(1); //WRITE
+		out_data.push_back((uint8_t)reg);
+		out_data.push_back(length);
+		serial_.flushInput();
+		serial_.write(out_data);
+		serial_.flushOutput();
+
+		std::vector<uint8_t> data_in;
+		serial_.read(data_in,2+length);
+
+	if(data[0]==0xBB && data[1]==length)
+	{
+		memcpy(data,data_in.data()+2,length);
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
 
 }/* namespace h4r_bosch_bno055_uart */
--- src/BoschBno055Uart.h
+++ src/BoschBno055Uart.h
@@ -27,6 +27,210 @@ namespace h4r_bosch_bno055_uart
 class BoschBno055Uart
 {
 private:
+
+
+	typedef enum
+	{
+
+		PAGE_ID=0x07,
+
+		//PAGE 0
+		PAGE0_CHIP_ID=0,
+		PAGE0_ACC_ID,
+		PAGE0_MAG_ID,
+		PAGE0_GYR_ID,
+		PAGE0_SW_REV_ID_LSB,
+		PAGE0_SW_REV_ID_MSB,
+		PAGE0_BL_Rev_ID,
+		PAGE0_Page_ID,
+		PAGE0_ACC_DATA_X_LSB,
+		PAGE0_ACC_DATA_X_MSB,
+		PAGE0_ACC_DATA_Y_LSB,
+		PAGE0_ACC_DATA_Y_MSB,
+		PAGE0_ACC_DATA_Z_LSB,
+		PAGE0_ACC_DATA_Z_MSB,
+		PAGE0_MAG_DATA_X_LSB,
+		PAGE0_MAG_DATA_X_MSB,
+		PAGE0_MAG_DATA_Y_LSB,
+		PAGE0_MAG_DATA_Y_MSB,
+		PAGE0_MAG_DATA_Z_LSB,
+		PAGE0_MAG_DATA_Z_MSB,
+		PAGE0_GYR_DATA_X_LSB,
+		PAGE0_GYR_DATA_X_MSB,
+		PAGE0_GYR_DATA_Y_LSB,
+		PAGE0_GYR_DATA_Y_MSB,
+		PAGE0_GYR_DATA_Z_LSB,
+		PAGE0_GYR_DATA_Z_MSB,
+		PAGE0_EUL_Heading_LSB,
+		PAGE0_EUL_Heading_MSB,
+		PAGE0_EUL_Roll_LSB,
+		PAGE0_EUL_Roll_MSB,
+		PAGE0_EUL_Pitch_LSB,
+		PAGE0_EUL_Pitch_MSB,
+		PAGE0_QUA_Data_w_LSB,
+		PAGE0_QUA_Data_w_MSB,
+		PAGE0_QUA_Data_x_LSB,
+		PAGE0_QUA_Data_x_MSB,
+		PAGE0_QUA_Data_y_LSB,
+		PAGE0_QUA_Data_y_MSB,
+		PAGE0_QUA_Data_z_LSB,
+		PAGE0_QUA_Data_z_MSB,
+		PAGE0_LIA_Data_X_LSB,
+		PAGE0_LIA_Data_X_MSB,
+		PAGE0_LIA_Data_Y_LSB,
+		PAGE0_LIA_Data_Y_MSB,
+		PAGE0_LIA_Data_Z_LSB,
+		PAGE0_LIA_Data_Z_MBS,
+		PAGE0_GRV_Data_X_LSB,
+		PAGE0_GRV_Data_X_MSB,
+		PAGE0_GRV_Data_Y_LSB,
+		PAGE0_GRV_Data_Y_MSB,
+		PAGE0_GRV_Data_Z_LSB,
+		PAGE0_GRV_Data_Z_MSB,
+		PAGE0_TEMP,
+		PAGE0_CALIB_STAT,
+		PAGE0_ST_RESULT,
+		PAGE0_INT_STA,
+		PAGE0_SYS_CLK_STATUS,
+		PAGE0_SYS_STATUS,
+		PAGE0_SYS_ERR,
+		PAGE0_UNIT_SEL,
+		PAGE0_Reserved,
+		PAGE0_OPR_MODE,
+		PAGE0_PWR_MODE,
+		PAGE0_SYS_TRIGGER,
+		PAGE0_TEMP_SOURCE,
+		PAGE0_AXIS_MAP_CONFIG,
+		PAGE0_AXIS_MAP_SIGN,
+		//RESERVED
+		PAGE0_ACC_OFFSET_X_LSB=0x55,
+		PAGE0_ACC_OFFSET_X_MSB,
+		PAGE0_ACC_OFFSET_Y_LSB,
+		PAGE0_ACC_OFFSET_Y_MSB,
+		PAGE0_ACC_OFFSET_Z_LSB,
+		PAGE0_ACC_OFFSET_Z_MSB,
+		PAGE0_MAG_OFFSET_X_LSB,
+		PAGE0_MAG_OFFSET_X_MSB,
+		PAGE0_MAG_OFFSET_Y_LSB,
+		PAGE0_MAG_OFFSET_Y_MSB,
+		PAGE0_MAG_OFFSET_Z_LSB,
+		PAGE0_MAG_OFFSET_Z_MSB,
+		PAGE0_GYR_OFFSET_X_LSB,
+		PAGE0_GYR_OFFSET_X_MSB,
+		PAGE0_GYR_OFFSET_Y_LSB,
+		PAGE0_GYR_OFFSET_Y_MSB,
+		PAGE0_GYR_OFFSET_Z_LSB,
+		PAGE0_GYR_OFFSET_Z_MSB,
+		PAGE0_ACC_RADIUS_LSB,
+		PAGE0_ACC_RADIUS_MSB,
+		PAGE0_MAG_RADIUS_LSB,
+		PAGE0_MAG_RADIUS_MSB,
+
+		PAGE1_Page_ID=0x07,
+		PAGE1_ACC_Config,
+		PAGE1_MAG_Config,
+		PAGE1_GYR_Config_0,
+		PAGE1_GYR_Config_1,
+		PAGE1_ACC_Sleep_Config,
+		PAGE1_GYR_Sleep_Config,
+		PAGE1_Reserved,
+		PAGE1_INT_MSK,
+		PAGE1_INT_EN,
+		PAGE1_ACC_AM_THRES,
+		PAGE1_ACC_INT_SET,
+		PAGE1_ACC_HG_DURATION,
+		PAGE1_ACC_HG_THRES,
+		PAGE1_ACC_NM_THRES,
+		PAGE1_ACC_NM_SET,
+		PAGE1_GYR_INT_SET,
+		PAGE1_GYR_HR_X_SET,
+		PAGE1_GYR_DUR_X,
+		PAGE1_GYR_HR_Y_SET,
+		PAGE1_GYR_DUR_Y,
+		PAGE1_GYR_HR_Z_SET,
+		PAGE1_GYR_DUR_Z,
+		PAGE1_GYR_AM_THRES,
+		PAGE1_GYR_AM_SET,
+		//RESERVED
+		PAGE1_UNIQE_ID_B0=0x50,
+		PAGE1_UNIQE_ID_B1=0x51,
+		PAGE1_UNIQE_ID_B2=0x52,
+		PAGE1_UNIQE_ID_B3=0x53,
+		PAGE1_UNIQE_ID_B4=0x54,
+		PAGE1_UNIQE_ID_B5=0x55,
+		PAGE1_UNIQE_ID_B6=0x56,
+		PAGE1_UNIQE_ID_B7=0x57,
+		PAGE1_UNIQE_ID_B8=0x58,
+		PAGE1_UNIQE_ID_B9=0x59,
+		PAGE1_UNIQE_ID_BA=0x5A,
+		PAGE1_UNIQE_ID_BB=0x5B,
+		PAGE1_UNIQE_ID_BC=0x5C,
+		PAGE1_UNIQE_ID_BD=0x5D,
+		PAGE1_UNIQE_ID_BE=0x5E,
+		PAGE1_UNIQE_ID_BF=0x5F,
+		//RESERVED
+
+	}BNO055Register;
+
+	typedef enum
+	{
+		OP_MODE_CONFIG,
+		OP_MODE_ACCONLY,
+		OP_MODE_MAGONLY,
+		OP_MODE_GYROONLY,
+		OP_MODE_ACCMAG,
+		OP_MODE_ACCGYRO,
+		OP_MODE_MAGGYRO,
+		OP_MODE_AMG,
+		OP_MODE_IMU,
+		OP_MODE_COMPASS,
+		OP_MODE_M4G,
+		OP_MODE_NDOF_FMC_OFF,
+		OP_MODE_NDOF,
+	}BNO055OpMode;
+
+
+	typedef enum
+	{
+		PWR_MODE_NORMAL,
+		PWR_MODE_LOW,
+		PWR_MODE_SUSPEND
+	}BNO055PwrMode;
+
+
+	typedef struct
+	{
+		struct
+		{
+			uint16_t AccelerationDataX;
+			uint16_t AccelerationDataY;
+			uint16_t AccelerationDataZ;
+			uint16_t MagnetometerDataX;
+			uint16_t MagnetometerDataY;
+			uint16_t MagnetometerDataZ;
+			uint16_t GyroscopeDataX;
+			uint16_t GyroscopeDataY;
+			uint16_t GyroscopeDataZ;
+			uint16_t HeadingData;
+			uint16_t RollData;
+			uint16_t PitchData;
+			uint16_t QuaternionwData;
+			uint16_t QuaternionxData;
+			uint16_t QuaternionyData;
+			uint16_t QuaternionzData;
+			uint16_t LinearAccelerationDataX;
+			uint16_t LinearAccelerationDataY;
+			uint16_t LinearAccelerationDataZ;
+			uint16_t GravityVectorDataX;
+			uint16_t GravityVectorDataY;
+			uint16_t GravityVectorDataZ;
+		}imu;
+		uint8_t temperature;
+		uint8_t calibration_status;
+	}ImuData;
+
+
+
 	ros::NodeHandle n_;
 	ros::NodeHandle nh_;
 	double rate_;
@@ -34,12 +238,34 @@ private:
 	sensor_msgs::Imu msg_imu_;
 	sensor_msgs::Imu msg_imu_raw_;
 	sensor_msgs::MagneticField msg_mag_;
-	sensor_msgs::Temperature Temperature_;
+	sensor_msgs::Temperature msg_temperature_;
 	serial::Serial serial_;
+	serial::Timeout serial_timeout_;
+
+
+
+	ros::Publisher pub_imu_;
+	ros::Publisher pub_imu_raw_;
+	ros::Publisher pub_mag_;
+	ros::Publisher pub_temp_;
+
+
+
+	bool writeIMU(BNO055Register reg,const void *data, unsigned length);
+	bool writeIMUByte(BNO055Register reg, uint8_t byte){return writeIMU(reg,&byte,1);}
+
+	bool readIMU(BNO055Register reg, uint8_t *data, unsigned length);
+
+
+
+
 
 public:
 	BoschBno055Uart();
 	virtual ~BoschBno055Uart();
+
+
+
 	int run();
 };/* class BoschBno055Uart */
 
